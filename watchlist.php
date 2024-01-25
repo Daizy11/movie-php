130247d06fb41a11d96ed781e3d34f484c364014
M. Habib Erdian <habib.erdian@gmail.com>
2024-01-25 17:34:17 +0700

    header and footer position fixed

--- watchlist.php
+++ watchlist.php
@@ -1,3 +1,35 @@
 <section class="watchlist">
-    
+    <article class="watchlist-title">
+        <p>Watchlist</p>
+    </article>
+    <section class="card-container">
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                       
+                    </section>
+                
+                </section>
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                       
+                    </section>
+                
+                </section>
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                    </section>
+                </section>
+            </section>
 </section>
