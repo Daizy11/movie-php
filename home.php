edb3ec24d09b040c75ea53c044367ac3f8d99aef
M. Habib Erdian <habib.erdian@gmail.com>
2024-01-25 10:18:15 +0700

    Created home page

---
+++ home.php
@@ -0,0 +1,84 @@
+<main class="content">
+            <article class="featured-today">
+                 <h5 class="featured-header">Featured Today</h5>
+            </article>
+            <section class="card-container">
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                        <button class="watch-list">
+                            +Watch List
+                        </button>
+                    </section>
+                
+                </section>
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                        <button class="watch-list">
+                            +Watch List
+                        </button>
+                    </section>
+                
+                </section>
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                        <button class="watch-list">
+                            +Watch List
+                        </button>
+                    </section>
+                </section>
+            </section>
+            <article class="top-10-wrapper">
+                 <h5 class="top-10">Top 10 on this week </h5>
+            </article>
+
+                <section class="card-container">
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                        <button class="watch-list">
+                            +Watch List
+                        </button>
+                    </section>
+                
+                </section>
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                        <button class="watch-list">
+                            +Watch List
+                        </button>
+                    </section>
+                
+                </section>
+                <section class="card-wrapper">
+                    <section class="card-upper">
+
+                    </section>
+                    <section class="card-down">
+                        <a href="" class="watch-now">Watch Now</a>
+                        <button class="watch-list">
+                            +Watch List
+                        </button>
+                    </section>
+                </section>
+            </section>
+        </main>
+      
---
+++ movie.php
@@ -0,0 +1,37 @@
+<section class="movie">
+    <section class="movie-header">
+        <p class="movie-title">Reacher</p>
+        <section class="movie-header-attribute-wrapper">
+            <section class="movie-header-attribute">
+                <p class="movie-header-title">Rating</p>
+                <section class="rating">
+                    <img src="gambar/Star.png" alt="" class="star">
+                    <p>4.5/5</p>
+                </section>
+            </section>
+            <section class="movie-header-attribute">
+                <p class="movie-header-title">Popularity</p>
+                <section class="popularity">
+                    <img src="gambar/Line_up@3x.png" alt="" class="star">
+                    <p>2</p>
+                </section>
+            </section>
+        </section>
+    </section>
+    <section class="movie-content">
+        <section class="thumbnail">
+
+        </section>
+        <section class="movie-video">
+
+        </section>
+    </section>
+    <section class="movie-genre">
+        <div class="genre">Action</div>
+        <div class="genre">Crime</div>
+    </section>
+    <section class="movie-team">
+        <section></section>
+    </section>
+
+</section>
