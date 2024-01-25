de16a002639fe04a67c33fff4ccf980bc1b4e4b5
M. Habib Erdian <habib.erdian@gmail.com>
2024-01-25 15:45:24 +0700

    already slice movie page

--- movie.php
+++ movie.php
@@ -5,14 +5,14 @@
             <section class="movie-header-attribute">
                 <p class="movie-header-title">Rating</p>
                 <section class="rating">
-                    <img src="gambar/Star.png" alt="" class="star">
+                    <img src="gambar/Star.png" alt="" class="star-image">
                     <p>4.5/5</p>
                 </section>
             </section>
             <section class="movie-header-attribute">
                 <p class="movie-header-title">Popularity</p>
                 <section class="popularity">
-                    <img src="gambar/Line_up@3x.png" alt="" class="star">
+                    <img src="gambar/Line_up@3x.png" alt="" class="star-image">
                     <p>2</p>
                 </section>
             </section>
@@ -31,7 +31,79 @@
         <div class="genre">Crime</div>
     </section>
     <section class="movie-team">
-        <section></section>
-    </section>
+        <section class="team">
+            <p>Creator</p>
+            <section class="team-name">
+                <p>Nick Santora</p>
+            </section>
 
+        </section>
+        <section class="team" style="border-bottom: 1px solid white;">
+            <p>Stars</p>
+            <section class="team-name" style="transform: translateX(1rem);">
+                <p>Maria Sten</p>
+                <p>Alan RitchSton</p>
+            </section>
+        </section>
+    </section>
+</section>
+<section class="cast">
+    <article class="top-cast">
+        <p>Top Cast</p>
+    </article>
+    <section class="star-wrapper">
+        <section class="star">
+            <img src="" alt="" class="stars-image">
+            <section class="star-name">
+                <p class="name-1">Alan Ritchson</p>
+                <p class="name-2">Jack Reacher</p>
+            </section>
+        </section>
+        <section class="star">
+            <img src="" alt="" class="stars-image">
+            <section class="star-name">
+                <p class="name-1">Alan Ritchson</p>
+                <p class="name-2">Jack Reacher</p>
+            </section>
+        </section> 
+        <section class="star">
+            <img src="" alt="" class="stars-image">
+            <section class="star-name">
+                <p class="name-1">Alan Ritchson</p>
+                <p class="name-2">Jack Reacher</p>
+            </section>
+        </section>
+        <section class="star">
+            <img src="" alt="" class="stars-image">
+            <section class="star-name">
+                <p class="name-1">Alan Ritchson</p>
+                <p class="name-2">Jack Reacher</p>
+            </section>
+        </section>
+    </section>
+    <section class="story">
+        <article class="storyline-title">
+            <p>Storyline</p>
+        </article>
+        <article class="storyline">
+            <p>When retired Military Police Officer Jack Reacher is arrested for a murder he did not commit,
+            he finds himself in the middle of a deadly conspiracy full of dirty cops, 
+            shady businessmen, and scheming politicians. With nothing but his wits, he must figure out 
+            what is happening in Margrave, Georgia.</p>
+        </article>
+    </section>
+    <section class="movie-detail-wrapper">
+        <section class="movie-detail">
+            <p class="movie-detail-title">Genres</p>
+            <section class="movie-detail-name" style="transform: translateX(1.3rem);">
+                <p>Nick Santora</p>
+            </section>
+        </section>
+        <section class="movie-detail" style="border-bottom: 1px solid #D9D9D9;">
+            <p class="movie-detail-title">Language</p>
+            <section class="movie-detail-name">
+                <p>English</p>
+            </section>
+        </section>
+    </section>
 </section>
